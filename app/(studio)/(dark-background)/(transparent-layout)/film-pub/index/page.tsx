2d9245a8cfda66b4878fddc0191b01eef56be7b2
antoineducrot <ducrot.antoine.en@gmail.com>
2025-09-11 12:12:54 +0200

    fix: index from folder creation

--- app/(studio)/(dark-background)/(transparent-layout)/film-pub/index/page.tsx
+++ app/(studio)/(dark-background)/(transparent-layout)/film-pub/index/page.tsx
@@ -4,7 +4,7 @@ import { filmsTypes } from '@/data/(studio)/films';
 import { metadataConfig } from '@/data/default-metadata';
 import { generateDefaultMetadata } from '@/helpers/generate-default-metadata.helper';
 
-import { FilmsPresentation } from '../_components/films-presentation';
+import { FilmsPresentation } from '../../_components/films-presentation';
 
 export const metadata: Metadata = generateDefaultMetadata(metadataConfig['/film-pub']);
 
